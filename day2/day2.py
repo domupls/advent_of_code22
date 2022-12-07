b3f735b91d4aa8c02318bd3be11908968df2b4ac
Domi Sidlova <domisidlova@gmail.com>
2022-12-07 17:54:50 +0100

    day2: implement fn for rps ending strategy

--- day2/day2.py
+++ day2/day2.py
@@ -7,13 +7,14 @@ win_shapes = {'A': 'Y', 'B': 'Z', 'C': 'X'}
 WIN = 6
 DRAW = 3
 
+# part one
 
-def rps_startegy():
+
+def rps_strategy():
     score = 0
     for line in stdin:
         l_split = line.split()
         op_move, my_move = l_split[0], l_split[1]
-        # print(op_move == win_shape[op_move])
         if op_move == same_shapes[my_move]:
             score += DRAW
         elif my_move == win_shapes[op_move]:
@@ -22,4 +23,23 @@ def rps_startegy():
     return score
 
 
-# print(rps_startegy())
+# print(rps_strategy())
+
+# part two
+def rps_end_strategy():
+    score = 0
+    for line in stdin:
+        l_split = line.split()
+        op_move, my_exit = l_split[0], l_split[1]
+        if my_exit == 'Y':
+            score += DRAW + score_shape[op_move]
+        elif my_exit == 'Z':
+            score += WIN + score_shape[same_shapes[win_shapes[op_move]]]
+        else:
+            my_move = [k for k, v in same_shapes.items() if v == op_move][0]
+            score += score_shape[[k for k,
+                                  v in win_shapes.items() if v == my_move][0]]
+    return score
+
+
+# print(rps_end_strategy())
