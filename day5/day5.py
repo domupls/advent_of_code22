8782c9adcc5d5101576ddfd9e3348c04c9cf0c83
domupls <domisidlova@gmail.com>
2022-12-18 11:57:28 +0100

    day5: rearrange multiple boxes fn

--- day5/day5.py
+++ day5/day5.py
@@ -30,3 +30,28 @@ def supply_stack():
 
 
 # print(supply_stack())
+
+# part two
+def multiple_supply_stack():
+    stack_list = []
+    message = ""
+    with open(filename) as f:
+        file = f.read()
+    stacks, instructions = file.split('\n\n')
+    for crates in stacks.split('\n'):
+        stack_list.append([crate for crate in crates[1::4]])
+    transposed_arr = [list(stack) for stack in zip(*stack_list[:-1])]
+    for stack in transposed_arr:
+        while ' ' in stack:
+            stack.remove(' ')
+    for ins in instructions.split('\n'):
+        res = [int(i) for i in ins.split() if i.isdigit()]
+        count, from_, to = res[0], res[1], res[2]
+        chars = transposed_arr[from_-1][:count]
+        transposed_arr[from_-1] = transposed_arr[from_-1][count:]
+        transposed_arr[to-1] = chars + transposed_arr[to-1]
+    for i in transposed_arr:
+        message += i[0]
+    return message
+
+print(multiple_supply_stack())
